38bd1e6e4965c43c551c83fae6efbf7ce9a8d430
Pawel Piatek <pawelpiatek98@gmail.com>
2020-02-20 19:53:05 +0100

    added blog page

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from "gatsby";
+
+const NavigationWrapper = styled.div`
+
+`;
+
+const Navigation = styled.ul`
+
+
+`;
+
+const NavigationItem = styled.li`
+
+
+`;
+
+const Navigation = () =>(
+  <NavigationWrapper>
+    <Navigation>
+
+    </Navigation>
+  </NavigationWrapper>
+);
+
+export default Navigation;
---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BlogPageWrapper = styled.div`
+
+`;
+
+const Blog = () =>(
+  <BlogPageWrapper>
+    <h1>Blog page</h1>
+  </BlogPageWrapper>
+);
+
+export default Blog;
