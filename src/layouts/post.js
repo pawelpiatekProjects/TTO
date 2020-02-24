a8ebad7919533da1a003a485f90e06b412d9f9ae
Pawel Piatek <pawelpiatek98@gmail.com>
2020-02-24 16:34:49 +0100

    post basic layout

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -8,34 +8,54 @@ export const querry = graphql`
         datoCmsArticle(id: {eq: $id}) {
             title
             author
-            featuredImage{
+            featuredImage {
                 fixed{
                     ...GatsbyDatoCmsFixed_tracedSVG
                 }
             }
-#            articleContent{
-#                ... on DatoCmsParagraph{
-#                    paragraphContent
-#                    id
-#                }
-#                ... on DatoCmsHeader{
-#                    headerContent
-#                    id
-#                }
-#                ... on DatoCmsImage{
-#                    imageData{
-#                        fixed{
-#                            ...GatsbyDatoCmsFixed_tracedSVG
-#                        }
-#                    }
-#                    id
-#                }
-#            }
+            articleContent{
+                ... on DatoCmsParagraph{
+                    paragraphContent
+                    id
+                }
+                ... on DatoCmsHeader{
+                    headingContent
+                    id
+                }
+                ... on DatoCmsImage{
+                    imageData{
+                        fixed{
+                            ...GatsbyDatoCmsFixed_tracedSVG
+                        }
+                    }
+                    id
+                }
+            }
         }
     }
 `;
 
 const PostLayoutWrapper = styled.div`
+
+`;
+
+const PostLatoutTitle = styled.h1`
+
+`;
+
+const PostLayoutAuthor = styled.h3`
+
+`;
+
+const ArticleContentHeader = styled.h2`
+
+`;
+
+const ArticleContentParagraph = styled.p`
+
+`;
+
+const ArticleContentImage = styled(Image)`
 
 `;
 
@@ -45,6 +65,22 @@ const PostLayout = ({data}) =>(
     <h1>{data.datoCmsArticle.title}</h1>
     <h1>{data.datoCmsArticle.author}</h1>
     {/*<Image fixed={data.featuredImage.fixed}/>*/}
+    <div>{data.datoCmsArticle.articleContent.map(el=>{
+      const key = el.__typename
+      switch (key) {
+        case "DatoCmsHeader": {
+          return <h1 key={el.id}>{el.headingContent}</h1>
+        }
+        case "DatoCmsParagraph": {
+          return <p key={el.id}>{el.paragraphContent}</p>
+        }
+        case "DatoCmsImage": {
+          return <Image key={el.id} fixed={el.imageData.fixed}/>
+        }
+        default: return null;
+      }
+    })}</div>
+
   </PostLayoutWrapper>
 );
 
