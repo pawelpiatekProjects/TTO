7a0d21437964b7e453afef4c1dcb895fd568aca8
Pawel Piatek <pawelpiatek98@gmail.com>
2020-02-27 21:04:24 +0100

    added basic contact form layout

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -1,10 +1,90 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Formik} from 'formik';
+import * as colors from '../../assets/styles/variables';
 
 const ContactFormWrapper = styled.div`
 
 `;
+
+const Form = styled.form`
+width: 60%;
+margin: 10rem auto;
+`;
+
+const Input = styled.input`
+display: block;
+width: 80%;
+padding: 1rem;
+margin-bottom: 2rem;
+outline: none;
+border: 3px solid ${colors.darkGray};
+`;
+
+const TextArea = styled.textarea`
+display: block;
+margin-bottom: 2rem;
+width: 100%;
+border: 3px solid ${colors.darkGray};
+outline: none;
+`;
+
+const Label = styled.label`
+font-weight: 700;
+font-size: 1.8rem;
+
+`;
+
+const Button = styled.button`
+border: none;
+background-color: ${colors.lightGray2};
+padding: 1rem 1.8rem;
+font-weight: 700;
+position: relative;
+transition: all .3s;
+margin-left: .8rem;
+
+&::before{
+content: '';
+display: block;
+position: absolute;
+top: -.8rem;
+left: -.8rem;
+width: 2rem;
+height: 2rem;
+border-top: 3px solid ${colors.darkGray};
+border-left: 3px solid ${colors.darkGray};
+transition: all .3s;
+}
+
+&::after{
+content: '';
+display: block;
+position: absolute;
+bottom: -.8rem;
+right: -.8rem;
+width: 2rem;
+height: 2rem;
+border-bottom: 3px solid ${colors.darkGray};
+border-right: 3px solid ${colors.darkGray};
+transition: all .3s;
+}
+
+&:hover{
+background-color: ${colors.white};
+color: ${colors.darkGray};
+}
+
+&:hover:before,
+&:hover:after{
+width: 100%;
+height: 100%;
+}
+
+
+`;
+
+
 
 const ContactForm = () => (
   <ContactFormWrapper>
@@ -38,8 +118,9 @@ const ContactForm = () => (
           isSubmitting,
           /* and other goodies */
         }) => (
-        <form onSubmit={handleSubmit}>
-          <input
+        <Form onSubmit={handleSubmit}>
+          <Label>Imię i nazwisko</Label>
+          <Input
             type="email"
             name="email"
             onChange={handleChange}
@@ -47,7 +128,8 @@ const ContactForm = () => (
             value={values.email}
           />
           {errors.email && touched.email && errors.email}
-          <input
+          <Label>E-mail</Label>
+          <Input
             type="password"
             name="password"
             onChange={handleChange}
@@ -55,10 +137,12 @@ const ContactForm = () => (
             value={values.password}
           />
           {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
+          <Label>Wiadomość</Label>
+          <TextArea rows="7"></TextArea>
+          <Button type="submit" disabled={isSubmitting}>
+            Wyślij
+          </Button>
+        </Form>
       )}
     </Formik>
   </ContactFormWrapper>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,10 @@
 import React from "react"
 import styled from "styled-components"
 import Image from "gatsby-image"
-import ContactForm from "../components/contactForm/contactForm";
-import {graphql} from 'gatsby';
-import IntroHeader from '../components/introHeader/introHeader';
-import SocialIcons from '../components/socialIcons/socialIcons';
+import ContactForm from "../components/contactForm/contactForm"
+import { graphql } from "gatsby"
+import IntroHeader from "../components/introHeader/introHeader"
+import SocialIcons from "../components/socialIcons/socialIcons"
 
 
 const ContactPageWrapper = styled.div`
@@ -14,7 +14,7 @@ const ContactPageWrapper = styled.div`
   width: 55%;
   padding: 3rem;
  
-`;
+`
 
 const ContactFormWrapper = styled.div`
   
@@ -24,6 +24,21 @@ const ContactFormHeader = styled(IntroHeader)`
 
 `;
 
+const TopContainer = styled.div`
+position: relative;
+margin-top: 14rem;
+  h1{
+  display: inline-block;
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 0;
+  }
+  div{
+  display: inline-block;
+  margin-top: 2rem;
+  }
+`;
 
 
 const ContactPaggeImage = styled(Image)`
@@ -33,21 +48,23 @@ right: 0;
 height: 100vh;
 width: 45%;
 
-`;
+`
 
-const ContactPage = ({data}) => (
+const ContactPage = ({ data }) => (
   <>
-  <ContactPageWrapper>
-    <SocialIcons/>
-    <ContactFormHeader>Kontakt</ContactFormHeader>
-    <ContactFormWrapper>
-
-    </ContactFormWrapper>
-  </ContactPageWrapper>
+    <ContactPageWrapper>
+      <TopContainer>
+        <SocialIcons/>
+        <ContactFormHeader>Kontakt</ContactFormHeader>
+      </TopContainer>
+      <ContactFormWrapper>
+        <ContactForm/>
+      </ContactFormWrapper>
+    </ContactPageWrapper>
     <ContactPaggeImage fluid={data.file.childImageSharp.fluid}/>
 
   </>
-);
+)
 
 export const contactQuerry = graphql`
     {
@@ -59,6 +76,6 @@ export const contactQuerry = graphql`
             }
         }
     }
-`;
+`
 
 export default ContactPage
